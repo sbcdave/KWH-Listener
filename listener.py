3a2d1da3f962763ae30e0bb6bb57f66eebdbc564
Dave <ggoldsmith@gmail.com>
2019-03-03 01:32:59 +0000

    Tested and adjusted to work

--- listener.py
+++ listener.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import socket
 import signal
 import subprocess
@@ -6,12 +6,12 @@ import zipfile
 from _thread import *
 import threading
 
-logPath="/KWH-Listener/logs/listener.log"
+logPath="/KWH-Listener/listener.log"
 
 def signal_handler(signal, frame):
     s.close()
     cs.close()
-    exit(1)
+    exit()
 signal.signal(signal.SIGINT, signal_handler)
 
 # Logger
@@ -27,34 +27,27 @@ def unzip(path):
 
 # thread fuction
 def threaded(c):
-    while True:
-        # data received from client
-        data = c.recv(1024)
-        if not data:
-            print('Bye')
-             
-            # lock released on exit
-            print_lock.release()
-            break
- 
-        # Do stuff with data
- 
-        # send back to client
-        c.send(data)
- 
+    data = str(c.recv(50000))
+    # Do stuff with data
+    log(data)
+
+    # Send comfirmation back to datalogger
+    c.send(bytes("@888", "utf-8"))
+    c.send(bytes(data, "utf-8"))
+
     # connection closed
     c.close()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = ''
-port = 11001
+port = 11003
 
 try:
     s.bind((host, port))
 except:
     log("Port "+str(port)+" in use\n")
-    exit(1)
-    
+    exit()
+
 log("Listening...\n")
 
 s.listen(1)
@@ -64,25 +57,8 @@ while True:
     # Waits for a command
     cs,addr = s.accept()
 
-    # Beginning to process received command
-    cmd = str(cs.recv(10))
-    log("Received: "+cmd+"\n")
-
-    # Pass data to parser
-    #execfile("/KWH/datalogger/config/pyvars.py")
-    #subprocess.Popen("/KWH/datalogger/transceive/ttyAMA0_setup.sh")
-   
-    # Send comfirmation back to datalogger
-    cs.send(bytes("@888", "utf-8")) #KP standard - only used for backwards compatibility
-    
     # Start a new thread and return its identifier
-    start_new_thread(threaded, (c,))
-    s.close()
-    
-    # Close client connection
-    cs.close()
-    log("Client connection closed\n")
-s.close()
+    start_new_thread(threaded, (cs,))
 
 if __name__ == '__main__':
     Main()
